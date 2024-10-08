202230958a159f8c1cceff39e1e9939510f27c9c
Hariram Raghuraman <168756823+hr6174ram@users.noreply.github.com>
2024-10-08 18:35:48 +0530

    Add files via upload

---
+++ Move It website/this.js
@@ -0,0 +1,12 @@
+alert("Welcome to Move It");
+
+/*Keywords
+String - "Hello"
+Number - 123
+Boolean - true false*/
+
+alert(2+3);
+typeof(123);
+
+//javascript variables
+prompt("what's your name?")
